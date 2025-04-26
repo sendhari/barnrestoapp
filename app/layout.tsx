f874bb70b591010f1bc9e6c08533f6e106558442
sendhari <hariharansg@gmail.com>
2025-04-26 13:27:48 +0530

    Fix deployment configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: 'Bar Management System',
   description: 'Bar Management System',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
-  manifest: '/manifest.json',
+  manifest: '/barnrestoapp/manifest.json',
   themeColor: '#312e81',
 }
 
