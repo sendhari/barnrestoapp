c70b55530ed6bcbe30163abdc094b0918ef688f8
sendhari <hariharansg@gmail.com>
2025-04-26 10:22:38 +0530

    Update Next.js configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
   basePath: '/barnrestoapp',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
